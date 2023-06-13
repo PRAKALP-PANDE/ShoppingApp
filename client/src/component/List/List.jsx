e6233b8e3c5586f25dfcae472defb643e7c9653c
PRAKALP-PANDE <prakalpsp16@gmail.com>
2023-06-14 00:54:22 +0530

    Added categoires

--- client/src/component/List/List.jsx
+++ client/src/component/List/List.jsx
@@ -1,52 +1,23 @@
 import React from 'react';
 import "./List.scss";
 import Card from "../Card/Card"
+import useFetch from '../../hooks/useFetch';
 
-const List = () => {
-    const data = [
-        {
-            id: 1,
-            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmsW6WkQAMyjSRsG-_eDBrWN9-TnNTpqxUolQ4cFK0dw&usqp=CAU&ec=48600112",
-            img2: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR_wbpUH2uHcNidiE1LW33JDLkHOPR4Gu-pNDIGKRhimA&usqp=CAU&ec=48600112",
-            title: "Long Sleeve Graphic T-shirt",
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 2,
-            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt1B1lWqGo6ImEL486dCDgBDGuulF2ScUY8Q&usqp=CAU",
-            img2: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7l-mfEyYBtLrdL4tD7sgMCHOVyadWnuOLAg&usqp=CAU",
-            title: "Long Sleeve Graphic T-shirt",
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 3,
-            img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6QorXLr7O2hllhZvNTnZL9c4JXlFRFyxPPEodNeZheg&usqp=CAU&ec=48600112",
-            img2: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_YRHsIWqqEE_i21fCloCJSiCLzGAQuucvWnX-kuzm0Q&usqp=CAU&ec=48600112",
-            title: "Long Sleeve Graphic T-shirt",
-            isNew: false,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 4,
-            img: "https://images.unsplash.com/photo-1589851287595-680777e79fda?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MjB8NTAzOTA3OHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
-            img2: "https://images.unsplash.com/photo-1589742117142-4c08de5aabcf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTl8NTAzOTA3OHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
-            title: "Long Sleeve Graphic T-shirt",
-            isNew: false,
-            oldPrice: 19,
-            price: 12,
-        },
-    ];
+const List = ({ subCats, maxPrice, sort, catId }) => {
+
+    const { data, loading, error } = useFetch(
+        `/products?populate=*&[filter][categories][id]=${catId}${subCats.map(
+            item=> `&[filters][sub_categories][id][$eq]=${item}`
+            )}&[filters][price][$ite]=${maxPrice}&sort=price:${sort}`
+    );
+
 
     return (
         <div className='list'>
-        {/* {data?.map(item=> (
-            <Card item={item} id={item.id} />
-        ))}  */}
+            {loading ? "loading" 
+            : data?.map(item => (
+                <Card item={item} id={item.id} />
+            ))}
         </div>
     )
 }
--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,55 +1,65 @@
 import React, { useState } from 'react';
-import {useParams} from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "./Products.scss";
 import List from '../../component/List/List';
+import useFetch from "../../hooks/useFetch";
 
 const Products = () => {
 
   const catId = parseInt(useParams().id)
   const [maxPrice, setMaxPrice] = useState(1000)
   const [sort, setSort] = useState(null)
+  const [selectedSubCats, setSelectedSubCats] = useState([])
+
+  const { data, loading, error } = useFetch(`/sub-categories?[filters][categories][id][$eq]=${catId}`)
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+
+    setSelectedSubCats(
+      isChecked
+        ? [...selectedSubCats, value]
+        : selectedSubCats.filter(item => item !== value)
+    );
+  };
+
 
   return (
     <div className="products">
       <div className="left">
         <div className="filterItem">
           <h2>Products Categories</h2>
-          <div className="inputItem">
-            <input type="checkbox" id='1' value={1} />
-            <label htmlFor="1">Shoes</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" id='2' value={2} />
-            <label htmlFor="2">Skirt</label>
-          </div>
-          <div className="inputItem">
-            <input type="checkbox" id='3' value={3} />
-            <label htmlFor="3">Coats</label>
-          </div>
+          {data?.map((item) => (
+            <div className="inputItem" key={item.id}>
+              <input type="checkbox" id={item.id} value={item.id} onChange={handleChange} />
+              <label htmlFor={item.id}>{item.attributes.title}</label>
+            </div>
+          ))}
         </div>
         <div className="filterItem">
           <h2>Filter by price</h2>
           <div className="inputItem">
             <span>0</span>
-            <input type="range" min={0} max={1000} onChange={(e)=> setMaxPrice(e.target.value)} />
+            <input type="range" min={0} max={1000} onChange={(e) => setMaxPrice(e.target.value)} />
             <span>{maxPrice}</span>
           </div>
         </div>
         <div className="filterItem">
           <h2>Sort by</h2>
           <div className="inputItem">
-            <input type="radio" id='asc' value="asc" name='price' onChange={e=>setSort('asc')} />
+            <input type="radio" id='asc' value="asc" name='price' onChange={e => setSort('asc')} />
             <label htmlFor="asc">Price (Lowest first)</label>
           </div>
           <div className="inputItem">
-            <input type="radio" id='desc' value="desc" name='price' onChange={e=>setSort('desc')} />
+            <input type="radio" id='desc' value="desc" name='price' onChange={e => setSort('desc')} />
             <label htmlFor="desc">Price (Highest first)</label>
           </div>
         </div>
       </div>
       <div className="right">
         <img className='catImg' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCLYoMdXQPGTQaC9190Tm2A3wIrtPrtf1PcYYBW7YUaQ&usqp=CAU&ec=48665701" alt="" />
-        <List catId={catId} maxPrice={maxPrice} sort={sort} />
+        <List catId={catId} maxPrice={maxPrice} sort={sort} subCats={selectedSubCats} />
       </div>
     </div>
   )
